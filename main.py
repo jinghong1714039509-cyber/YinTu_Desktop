57cc45093b156a36be7e02cf625450657deed46c
jinghong1714039509-cyber <jinghong1714039509@gmail.com>
2025-12-24 13:18:34 +0800

    0.0.4

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from app.ui.main_window import MainWindow
-from app.models.schema import init_db
 
-if __name__ == '__main__':
-    # 1. 初始化数据库
-    init_db()
-
-    # 2. 启动应用
+def main():
     app = QApplication(sys.argv)
     w = MainWindow()
     w.show()
-    
     sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
